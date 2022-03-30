29c21d298fecd288943ee2a6852e4c9f1951ade8
EHoldener <eholdener12@gmail.com>
2022-03-30 12:58:32 -0700

    had to google some code to figure out where I was going wrong with recursion in build tree, but build tree is working correctly now. Also created find functionality

--- script.rb
+++ script.rb
@@ -1,40 +1,43 @@
 class Tree
+  attr_accessor :root, :data
+
   def initialize(array)
-    @array = array
-    @root = build_tree(array)
+    @data = array.sort.uniq
+    @root = build_tree(data)
   end
 
   # builds the initial tree with recursion
   def build_tree(array)
-    array = merge_sort(array)
-    array = delete_dupes(array)
+    return nil if array.empty?
 
-    start_tree = array[0]
-    end_tree = array.length
+    middle = (array.size - 1) / 2
+    root_node = Node.new(array[middle])
 
-    if start_tree > end_tree
-      return nil
-    end
+    root_node.left_child = build_tree(array[0...middle])
+    root_node.right_child = build_tree(array[(middle + 1)..-1])
 
-    mid_tree = (start_tree + (end_tree -start_tree)) / 2
-
-    root = array[mid_tree]
-    left_child = build_tree(array[start_tree..mid_tree-1])
-    right_child = build_tree(array[mid_tree+1..end_tree])
-
-    node = Node.new(root, left_child, right_child)
+    root_node
   end
 
   # accepts a value to insert in the tree
   def insert(value)
   end
-  
+
   # accepts a value to delete in the tree
   def delete(value)
   end
 
   # accepts a value, returns node with that value
   def find(value)
+    root = @root
+    while value != root.data
+      if value > root.data
+        root = root.right_child
+      else
+        root = root.left_child
+      end
+    end
+    root
   end
 
   # accepts a block
@@ -60,7 +63,8 @@ class Tree
   end
 
   # checks if tree is balanced
-  # a balanced tree is defined as one where the difference between heights of the left subtree and right subtree of every node is not more than 1
+  # a balanced tree is defined as one where the difference between heights of the 
+  # left subtree and right subtree of every node is not more than 1
   def balanced?
   end
 
@@ -68,61 +72,18 @@ class Tree
   # traverses the tree to create an array, then passes array to build_tree
   def rebalance
   end
-
-  def merge_sort(arr)
-    if arr.length < 2
-      return arr
-    else
-      full_arr = []
-      left_arr = merge_sort(arr[0..arr.length/2-1])
-      right_arr = merge_sort(arr[arr.length/2..])
-      i_l = 0
-      i_r = 0
-      while i_l < left_arr.length && i_r < right_arr.length
-        if left_arr[i_l] < right_arr[i_r]
-          full_arr.push(left_arr[i_l])
-          i_l += 1
-        else
-          full_arr.push(right_arr[i_r])
-          i_r += 1
-        end
-      end
-      if i_l == left_arr.length
-        full_arr.push(right_arr[i_r..])
-      else
-        full_arr.push(left_arr[i_l..])
-      end
-      full_arr = full_arr.flatten
-    end
-  end
-
-  def delete_dupes(arr)
-    p "array = #{arr}"
-    last_value = nil
-    i = 0
-    while i < arr.length
-      value = arr[i]
-      p "value = #{value}"
-      p "last_value = #{last_value}"
-      if last_value == value
-        arr.delete_at(i)
-        arr
-        next
-      end
-      last_value = value
-      i += 1
-    end
-    arr
-  end
 end
 
 class Node
-  include Comparable
-  def initialize(data, left_child = nil, right_child = nil)
+  attr_accessor :left_child, :right_child, :data
+
+  def initialize(data)
     @data = data
-    @left_child = left_child
-    @right_child = right_child
+    @left_child = nil
+    @right_child = nil
   end
 end
 
-
+arr = [1, 3, 5, 6, 1, 7]
+tree = Tree.new(arr)
+p tree.find(6)
